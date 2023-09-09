daf03ced97954e08c5fb8e866ee006c4eb959431
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-09-09 08:50:18 -0600

    Se agrega la capa de service, la interfaz y la clase

---
+++ src/main/java/com/app/web/servicio/EstudianteServicioImpl.java
@@ -0,0 +1,14 @@
+package com.app.web.servicio;
+
+import com.app.web.entidad.Estudiante;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstudianteServicioImpl implements EstudianteServicio{
+    @Override
+    public List<Estudiante> listarTodosLosEstudiantes() {
+        return null;
+    }
+}
