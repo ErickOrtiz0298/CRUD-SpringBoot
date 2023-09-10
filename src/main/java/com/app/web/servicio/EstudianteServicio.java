cf0a1d230c6bdc5a51eb76eb51ef3a03ef9d094c
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-09-09 23:39:52 -0600

    Se agregan los metodos obtener estudiante por id, actualizar estudiante y eliminar estudiante

--- src/main/java/com/app/web/servicio/EstudianteServicio.java
+++ src/main/java/com/app/web/servicio/EstudianteServicio.java
@@ -6,4 +6,10 @@ import java.util.List;
 public interface EstudianteServicio {
     public List<Estudiante> listarTodosLosEstudiantes();
     public Estudiante guardarEstudiante(Estudiante estudiante);
+
+    public Estudiante obtenerEstudiantePorId(Long id);
+
+    public Estudiante actualizarEstudiante(Estudiante estudiante);
+    public void eliminarEstudiante(Long id);
+
 }
